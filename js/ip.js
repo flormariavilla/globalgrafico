25d48a36d05767e79ef4e926dab9c3bfe1a951e9
flormariavilla <flormevs170@gmail.com>
2023-08-03 10:37:56 -0300

    updating project

---
+++ js/ip.js
@@ -0,0 +1,12 @@
+
+alert("Hola mundo");
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+const boton = document.querySelector('#boton');
+
+boton.addEventListener("", function(){ alert("hola")})
+/*
+boton.addEventListener('click', ()=> {
+     Notification.requestPermission()
+     .then(resultado => alert('El resultado es'+ resultado ))
+   
+});*/
